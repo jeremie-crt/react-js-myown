3732f853ec78706f854458cad48835763941fd70
JeremieC <certain.j@live.fr>
2021-06-09 17:54:04 +0200

    add Context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,55 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import React, {Component} from "react";
+import React, {Component, createContext, useContext} from "react";
+import {BrowserRouter, Link, Route, Switch, Redirect} from "react-router-dom";
 
 import {Button} from "react-bootstrap";
+
 import Register from "./components/Register";
+import Login from "./components/Login";
+
 import HeaderNav from "./components/HeaderNav";
 import ContentHtml from "./components/ContentHtml";
-import Login from "./components/Login";
-import {BrowserRouter, Link, Route, Switch, Redirect } from "react-router-dom";
+import sessionContext from "./MyContext";
 
+const Articles = () => {
+    //let auth = useSessionAuth()
+    return (
+        <sessionContext.Consumer>
+            {
+                value => {
+                    console.log('Articles VALUE', value.state)
+                    let state = value.state ? 'true' : 'false'
+                    return <p>Session state: {state}</p>
+                }
+            }
+        </sessionContext.Consumer>
+    )
+}
+
+const About = () => {
+    let auth = useSessionAuth()
+    console.log('auth About', auth)
+    return (
+        <p>About</p>
+    )
+}
+
+const Account = () => <p>Account</p>
+
+const Logout = ({logout, session}) => {
+    let auth = useSessionAuth()
+    if (auth.state) {
+        logout()
+    }
+
+    return <Redirect to='/'/>
+}
+
+
+function useSessionAuth() {
+    return useContext(sessionContext)
+}
 
 class App extends Component {
     constructor(props) {
@@ -17,7 +58,7 @@ class App extends Component {
             users: {},
             session: {
                 started: false,
-                user: { id: '', username: ''}
+                user: {id: '', username: ''}
             }
         }
 
@@ -33,107 +74,132 @@ class App extends Component {
             date: new Date().toLocaleDateString()
         }
 
-        const users = { ...this.state.users }
+        const users = {...this.state.users}
         let id = `user-${Date.now()}`
         users[id] = user
 
-        this.setState({ users })
+        this.setState({users})
         localStorage.setItem('listUsers', JSON.stringify(users))
         this.logUser(id, users)
     }
 
     logUser(userId, list) {
-        const session = { ...this.state.session }
+        const session = {...this.state.session}
 
         const user = list[userId]
         session.started = true
         session.user.id = userId
         session.user.username = user.username
 
-        this.setState({ session })
+        this.setState({session})
     }
 
     logOut() {
-        let session = { ...this.state.session }
+        let session = {...this.state.session}
         session.started = false
-        session.user = { id: '', username: ''}
+        session.user = {id: '', username: ''}
 
-        this.setState({ session })
+        this.setState({session})
+    }
+
+    useProvideSession = () => {
+        let session = {...this.state.session}
+        let state = session.started
+        let user = session.user
+
+        return {
+            state,
+            user
+        }
     }
 
     render() {
-
-        const Articles = () => <p>Articles</p>
-        const About = () => <p>About</p>
-        const Account = () => <p>Account</p>
-
-        const Logout = ({ logout, session }) => {
-            if(session.started) {
-                logout()
-            }
-
-            return <Redirect to='/' />
+        const SessionProvider = ({children}) => {
+            let sessionAuth = this.useProvideSession()
+            return (
+                <sessionContext.Provider value={sessionAuth}>
+                    {children}
+                </sessionContext.Provider>
+            )
         }
-
         return (
 
             <BrowserRouter>
+                <SessionProvider>
 
-                <div className='container'>
-                    <HeaderNav />
+                    <div className='container'>
+                        <HeaderNav/>
 
-                    <ContentHtml />
+                        <ContentHtml/>
 
-                    <div className="row">
-                        <h1 className='mt-2'>Hello</h1>
-                        <p>Phosfluorescently extend impactful process improvements rather than open-source quality vectors. Efficiently syndicate stand-alone quality vectors for innovative solutions. Authoritatively morph virtual infrastructures before diverse testing.</p>
-                    </div>
-
-                    <Switch>
-                        <Route path='/articles'>
-                            <Articles />
-                        </Route>
-                        <Route path='/about'>
-                            <About />
-                        </Route>
-                        <Route path='/myaccount'>
-                            <Account />
-                        </Route>
-                        <Route path='/signin'>
-                            <Login
-                                listUsers={this.state.users}
-                                saveNewUser={this.saveNewUser}
-                                logUser={this.logUser}
-                                session={this.state.session}
-                            />
-                        </Route>
-                        <Route path='/signup'>
-                            <Register
-                                listUsers={this.state.users}
-                                saveNewUser={this.saveNewUser}
-                                session={this.state.session}
-                            />
-                        </Route>
-                        <Route path='/signout'>
-                            <Logout
-                                logout={this.logOut}
-                                session={this.state.session}
-                            />
-                        </Route>
-                    </Switch>
-
-                    <section className='mt-5'>
                         <div className="row">
-                            <p>Collaboratively recaptiualize just in time total linkage via exceptional resources. Continually plagiarize extensible results rather than ethical channels. Rapidiously transition interactive infomediaries via leveraged functionalities. Proactively impact multimedia based metrics after covalent functionalities. Seamlessly transition front-end partnerships via resource maximizing ideas.
-
-                                Monotonectally formulate collaborative leadership with inexpensive models. Progressively transform timely convergence whereas standards compliant convergence. Assertively build go forward testing procedures before flexible ideas. Phosfluorescently synthesize out-of-the-box applications without multimedia based benefits. Assertively monetize prospective results via customized collaboration and idea-sharing.
-
-                                Efficiently re-engineer interdependent expertise through B2B functionalities. Dynamically morph leveraged growth strategies without equity invested manufactured products. Progressively mesh cross-unit niches rather than efficient communities. Quickly engineer multidisciplinary models after robust convergence. Objectively transition parallel partnerships after innovative meta-services.
-</p>
-                            <Button className='mb-5'>CLICK ON IT</Button>
+                            <h1 className='mt-2'>Hello</h1>
+                            <p>Phosfluorescently extend impactful process improvements rather than open-source quality
+                                vectors. Efficiently syndicate stand-alone quality vectors for innovative solutions.
+                                Authoritatively morph virtual infrastructures before diverse testing.</p>
                         </div>
-                    </section>
-                </div>
+
+                        <Switch>
+                            <Route path='/articles'>
+                                <Articles/>
+                            </Route>
+                            <Route path='/about'>
+                                <About/>
+                            </Route>
+                            <Route path='/myaccount'>
+                                <Account/>
+                            </Route>
+                            <Route path='/signin'>
+                                <Login
+                                    listUsers={this.state.users}
+                                    saveNewUser={this.saveNewUser}
+                                    logUser={this.logUser}
+                                    session={this.state.session}
+                                />
+                            </Route>
+                            <Route path='/signup'>
+                                <Register
+                                    listUsers={this.state.users}
+                                    saveNewUser={this.saveNewUser}
+                                    session={this.state.session}
+                                />
+                            </Route>
+                            <Route path='/signout'>
+                                <Logout
+                                    logout={this.logOut}
+                                />
+                            </Route>
+                        </Switch>
+
+                        <section className='mt-5'>
+                            <div className="row">
+                                <p>Collaboratively recaptiualize just in time total linkage via exceptional resources.
+                                    Continually plagiarize extensible results rather than ethical channels. Rapidiously
+                                    transition interactive infomediaries via leveraged functionalities. Proactively
+                                    impact
+                                    multimedia based metrics after covalent functionalities. Seamlessly transition
+                                    front-end
+                                    partnerships via resource maximizing ideas.
+
+                                    Monotonectally formulate collaborative leadership with inexpensive models.
+                                    Progressively
+                                    transform timely convergence whereas standards compliant convergence. Assertively
+                                    build
+                                    go forward testing procedures before flexible ideas. Phosfluorescently synthesize
+                                    out-of-the-box applications without multimedia based benefits. Assertively monetize
+                                    prospective results via customized collaboration and idea-sharing.
+
+                                    Efficiently re-engineer interdependent expertise through B2B functionalities.
+                                    Dynamically morph leveraged growth strategies without equity invested manufactured
+                                    products. Progressively mesh cross-unit niches rather than efficient communities.
+                                    Quickly engineer multidisciplinary models after robust convergence. Objectively
+                                    transition parallel partnerships after innovative meta-services.
+                                </p>
+                                <Button className='mb-5'>CLICK ON IT</Button>
+                            </div>
+                        </section>
+                    </div>
+                </SessionProvider>
 
             </BrowserRouter>
         );
---
+++ src/MyContext.js
@@ -0,0 +1,5 @@
+import {createContext} from "react";
+
+const sessionContext = createContext(null)
+
+export default sessionContext;
